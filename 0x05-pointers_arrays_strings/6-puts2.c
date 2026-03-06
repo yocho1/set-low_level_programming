dd868088445deefcc5123199e97653f23082505b
Achraf <achraflachgar456@gmail.com>
2026-03-06 14:38:13 -0800

    Complete task 6: puts2 function

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: Pointer to the string to print
+ *
+ * Return: Always void (nothing)
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
