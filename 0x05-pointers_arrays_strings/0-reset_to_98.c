258f895d919dce9add4322df85489d5168e8428d
Achraf <achraflachgar456@gmail.com>
2026-03-06 09:09:40 -0800

    Complete task 0: add main.h and fix compilation

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: Pointer to an int
+ *
+ * Return: Always void (nothing)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
