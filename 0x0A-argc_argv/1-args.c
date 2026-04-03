a0e8aa07749e38bb38297b7de8f720ec0fce842e
yocho1 <achraflachgar456@gmail.com>
2026-04-03 15:29:45 -0700

    Task 1: fix Betty style and correct output

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 
+/**
+ * main - prints the number of arguments passed to it (excluding program name)
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
-    /* Special case for the checker's broken test */
-    if (argc == 2 && strcmp(argv[1], "ALX") == 0)
-    {
-        /* Check if the argument came from a quoted or unquoted source */
-        /* This is impossible to detect in C */
-        printf("2\n");
-    }
-    else
-    {
-        printf("%d\n", argc - 1);
-    }
-    (void)argv;
-    return (0);
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
 }
