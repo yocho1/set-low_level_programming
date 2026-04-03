106d5cfbeb389c011a95836c26c60d1994ba300a
yocho1 <achraflachgar456@gmail.com>
2026-04-03 15:06:12 -0700

    Complete task 2: args - print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector (array of strings)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	/* Loop through all arguments */
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
