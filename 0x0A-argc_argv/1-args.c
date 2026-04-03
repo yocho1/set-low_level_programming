fd5412294ce506e03018a2b7e82cbfde5df8d283
yocho1 <achraflachgar456@gmail.com>
2026-04-03 15:31:38 -0700

    Task 1: standard solution - print argc - 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed to it (including program name)
+ * main - prints the number of arguments passed to it
  * @argc: argument count
  * @argv: argument vector
  *
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
 (void)argv;
 return (0);
 }
