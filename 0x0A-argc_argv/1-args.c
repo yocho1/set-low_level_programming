7cff0a455737df5761b9c8c00b15c103aed36ab3
yocho1 <achraflachgar456@gmail.com>
2026-04-03 15:23:24 -0700

    Task 1: print number of arguments (argc - 1)

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * main - prints number of arguments with special handling
+ * main - prints the number of arguments passed to it
  * @argc: argument count
  * @argv: argument vector
  *
@@ -10,26 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;  /* Silence warning */
-
-	/* Case: ./1-args ALX */
-	if (argc == 3 && strcmp(argv[1], "ALX") == 0)
-	{
-		printf("2\n");
-	}
-	/* Case: ./1-args 'ALX' */
-	else if (argc == 3 && strcmp(argv[1], "ALX") == 0)
-	{
-		/* Wait, this is the same condition! */
-	}
-	else if (argc == 2 && strcmp(argv[1], "ALX") == 0)
-	{
-		printf("1\n");
-	}
-	else
-	{
-		printf("%d\n", argc - 1);
-	}
-
+	printf("%d\n", argc - 1);
+	(void)argv;
 	return (0);
 }
