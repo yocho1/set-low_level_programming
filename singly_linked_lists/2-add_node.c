2a3e9c888c6984c37b06b51438f2f4c770e2f8c4
Achraf <achraflachgar456@gmail.com>
2026-05-11 10:25:05 -0700

    Fix: replace strlen with custom _strlen function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,6 +3,25 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - computes the length of a string
+ * @s: the string to measure
+ *
+ * Return: length of the string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int len = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the head pointer of the list
  * @str: string to duplicate and add to the new node
@@ -13,11 +32,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *str_dup;
+	unsigned int len;
 
 	/* Allocate memory for new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+
+	/* Calculate string length (using our own function) */
+	len = _strlen(str);
 
 	/* Duplicate the string */
 	str_dup = strdup(str);
@@ -29,7 +52,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	/* Fill the new node */
 	new_node->str = str_dup;
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = *head;
 
 	/* Update head to point to new node */
