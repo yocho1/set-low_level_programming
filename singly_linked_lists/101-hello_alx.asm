1cccb8747ca9149ef641c00af745cdf4ce4a50ed
Achraf <achraflachgar456@gmail.com>
2026-05-29 16:32:29 -0700

    Add 101-hello_alx.asm: assembly program prints Hello, ALX

---
+++ singly_linked_lists/101-hello_alx.asm
@@ -0,0 +1,18 @@
+section .data
+    msg db "Hello, ALX", 10, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    mov rdi, msg
+    xor eax, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    ret
