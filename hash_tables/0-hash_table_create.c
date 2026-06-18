499c2f5ea488776295f465977d3d5b2790d96a20
Achraf <achraflachgar456@gmail.com>
2026-06-18 16:05:02 -0700

    Fix: remove count field and __packed from hash_table_s for correct 16400 byte allocation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,31 +1,24 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a new hash table
- * @size: The size of the array (number of buckets)
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
  *
- * Return: Pointer to the newly created hash table, or NULL on failure
+ * Return: A pointer to the new hash table, or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+        hash_table_t *ht;
 
-	if (size < 1)
-		return (NULL);
-
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
-
-	ht->array = calloc(size, sizeof(hash_node_t *));
-	if (ht->array == NULL)
-	{
-		free(ht);
-		return (NULL);
-	}
-
-	ht->size = size;
-	ht->count = 0;
-
-	return (ht);
+        ht = malloc(sizeof(hash_table_t));
+        if (ht == NULL)
+                return (NULL);
+        ht->array = calloc(size, sizeof(hash_node_t *));
+        if (ht->array == NULL)
+        {
+                free(ht);
+                return (NULL);
+        }
+        ht->size = size;
+        return (ht);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,35 +1,31 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
- * @key: The key, string
+ * @key: The key (string)
  * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * @next: A pointer to the next node of the list
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+        char *key;
+        char *value;
+        struct hash_node_s *next;
 } hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
  * @size: The size of the array
  * @array: An array of size @size
- * @count: Number of elements in the hash table
  */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
-	unsigned long int count;
-} __attribute__((packed)) hash_table_t;
+        unsigned long int size;
+        hash_node_t **array;
+} hash_table_t;
 
 /* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
